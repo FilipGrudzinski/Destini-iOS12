a75634755c8cfc1452d6214956f44ca84647ba15
FilipGrudzinski <grudzinski.filip@gmail.com>
2018-09-26 13:57:42 +0200

    Finished app with objected story

--- Destini/Model/Question.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Question.swift
-//  Destini
-//
-//  Created by Filip on 26/09/2018.
-//  Copyright © 2018 London App Brewery. All rights reserved.
-//
-
-import Foundation
---
+++ Destini/Model/Story.swift
@@ -0,0 +1,26 @@
+//
+//  Question.swift
+//  Destini
+//
+//  Created by Filip on 26/09/2018.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+class Story {
+    
+    let storyText: String
+    let answer1: String
+    let answer2: String
+    
+    init(text: String, answerText1: String, answerText2: String) {
+        
+        storyText = text
+        answer1 = answerText1
+        answer2 = answerText2
+        
+    }
+    
+}
